b74e4b2e7871b201f2e5c2563eca463506a19724
Masud karim <msmasud578@gmail.com>
2022-04-02 22:55:08 -0800

    Adding doc to the order entity class(model)

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * This the entity class for Order table
+ * 
+* */
 class Order extends Model
 {
     use HasFactory;
