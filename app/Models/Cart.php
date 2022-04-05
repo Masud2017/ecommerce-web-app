c63b6db570de8baede118b5d70eebaf1acfe91bd
Masud karim <msmasud578@gmail.com>
2022-04-05 13:57:17 -0800

    Adding cart model and migration and making it one to one relationship with user

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -1,21 +1,21 @@
 <?php
 
-// namespace App\Models;
+namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-// class Cart extends Model
-// {
-//     use HasFactory;
+class Cart extends Model
+{
+    use HasFactory;
 
-//     protected $fillable = ['user_id','quantity'];
+    protected $fillable = ['quantity'];
 
-//     public function user() {
-//         return $this->hasOne(User::class);
-//     }
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 
-//     public function products() {
-//         return $this->belongsToMany(Product::class,'products_cart');
-//     }
-// }
+    // public function products() {
+    //     return $this->belongsToMany(Product::class,'products_cart');
+    // }
+}
