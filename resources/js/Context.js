ddcfbc57b9d264a453bfe0e24556354dad262f06
Masud karim <msmasud578@gmail.com>
2022-04-08 19:12:12 -0800

    Adding context for global state management in front end

---
+++ resources/js/Context.js
@@ -0,0 +1,30 @@
+import {createContext,useReducer} from "react";
+
+export const AppContext = createContext();
+
+const initialState = {darkMode:true,isAuth:false};
+
+/**
+ *
+ * @param {*} state - global state of the application
+ * @param {*} action - action for determining state change
+ * @returns
+ */
+const globalReducer = (state, action)=> {
+    switch(action.type) {
+        case 'toggle':
+            return {darkMode : !state.darkMode};
+        default:
+            return state;
+    }
+}
+
+export const ContextProvider = (props)=> {
+    const [state, dispatch] = useReducer(globalReducer,initialState);
+
+    return (
+        <AppContext.Provider value={{state,dispatch}}>
+            {props.children}
+        </AppContext.Provider>
+    );
+}
